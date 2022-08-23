9207ba9472a01640c9cff4de2a563ccaf0c7930d
kawasemi70 <108577146+kawasemi70@users.noreply.github.com>
2022-08-23 12:05:35 +0000

    [finish]Bookers2 4-24

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+
+  devise_for :users
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:index,:show,:edit,:update,:create,:destroy]
+  resources :users, only: [:index,:show,:edit,:update]
+
+  get "home/about" => "homes#about"
 end
+
